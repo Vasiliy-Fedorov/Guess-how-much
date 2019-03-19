f2746f6b5bbb027f75d0cff1da74277e1a21f048
Vasiliy-Fedorov <zolotokrad@gmail.com>
2019-03-19 21:00:00 +0300

    фвÐadded new conditions

--- 1.js
+++ 1.js
@@ -5,8 +5,14 @@ function guess() {
     num = document.getElementById('innum').value;
     out = document.getElementById('out');
 
-    if (num > prntNum){
-        out.innerHTML = 'Введите число меньше!'
+    if (num > 10) {
+        out.innerHTML = 'Ого Вы умеете считать больше десяти! Но введите число от 0 до 10'
+    }
+    else if (num < 0) {
+        out.innerHTML = 'Введите положительное число число!'
+    }
+    else if (num > prntNum){
+            out.innerHTML = 'Введите число меньше!'
     }
     else if (num < prntNum){
         out.innerHTML = 'Введите число больше!'
